d6fa475f7a20b0063b79746d83caf0d93b3dc45c
vromahya <kvarun339@gmail.com>
2022-12-17 22:38:12 +0530

    'first tests done'

--- test.js
+++ test.js
@@ -1,1 +1,30 @@
 //Add your tests here
+
+const { describe, it } = require('mocha');
+const { getDataAsArray } = require('./auxillaryFunctions');
+const { assert } = require('console');
+const inputLines = [
+  'BALANCE MC1 600',
+  'BALANCE MC2 500 ',
+  'BALANCE MC3 50 ',
+  'BALANCE MC4 50 ',
+  'BALANCE MC5 200 ',
+  'CHECK_IN MC1 ADULT CENTRAL ',
+  'CHECK_IN MC2 SENIOR_CITIZEN CENTRAL ',
+  'CHECK_IN MC1 ADULT AIRPORT ',
+  'CHECK_IN MC3 KID AIRPORT ',
+  'CHECK_IN MC4 ADULT AIRPORT ',
+  'CHECK_IN MC5 KID AIRPORT ',
+  'PRINT_SUMMARY ',
+];
+describe('Unit tests', function () {
+  it('first output of getArray as data should be array', function () {
+    const { inputCardBalance, _ } = getDataAsArray(inputLines);
+    assert(Array.isArray(inputCardBalance));
+  });
+  it('second output of getArray as data should be array', function () {
+    const { _, inputPassengerCardsAndDestinations } =
+      getDataAsArray(inputLines);
+    assert(Array.isArray(inputPassengerCardsAndDestinations));
+  });
+});
