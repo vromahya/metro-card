3f9349488b76fb954fbcb6164f9f7f613753d34c
vromahya <kvarun339@gmail.com>
2022-12-17 22:54:42 +0530

    second unit tests

--- auxillaryFunctions.js
+++ auxillaryFunctions.js
@@ -24,10 +24,15 @@ const getDataAsArray = (inputLines) => {
   return { inputCardBalance, inputPassengerCardsAndDestinations };
 };
 const checkIfReturnJourney = (expenses, card_no) => {
-  const previosJourneys = expenses.filter(
+  console.log(expenses);
+  const previousJourneys = expenses.filter(
     (expense) => expense.card_no === card_no
   );
-  if (previosJourneys.length % 2 === 0) return false;
+  if (previousJourneys.length % 2 === 0) {
+    console.log('false');
+    return false;
+  }
+  console.log(true);
   return true;
 };
 const returnNewCardBalances = (inputCardBalance, expense, cardNo) => {
--- test.js
+++ test.js
@@ -1,23 +1,27 @@
 //Add your tests here
 
 const { describe, it } = require('mocha');
-const { getDataAsArray } = require('./auxillaryFunctions');
-const { assert } = require('console');
-const inputLines = [
-  'BALANCE MC1 600',
-  'BALANCE MC2 500 ',
-  'BALANCE MC3 50 ',
-  'BALANCE MC4 50 ',
-  'BALANCE MC5 200 ',
-  'CHECK_IN MC1 ADULT CENTRAL ',
-  'CHECK_IN MC2 SENIOR_CITIZEN CENTRAL ',
-  'CHECK_IN MC1 ADULT AIRPORT ',
-  'CHECK_IN MC3 KID AIRPORT ',
-  'CHECK_IN MC4 ADULT AIRPORT ',
-  'CHECK_IN MC5 KID AIRPORT ',
-  'PRINT_SUMMARY ',
-];
-describe('Unit tests', function () {
+const {
+  getDataAsArray,
+  checkIfReturnJourney,
+} = require('./auxillaryFunctions');
+const assert = require('assert');
+
+describe('Unit test for getDataAsArray', function () {
+  const inputLines = [
+    'BALANCE MC1 600',
+    'BALANCE MC2 500 ',
+    'BALANCE MC3 50 ',
+    'BALANCE MC4 50 ',
+    'BALANCE MC5 200 ',
+    'CHECK_IN MC1 ADULT CENTRAL ',
+    'CHECK_IN MC2 SENIOR_CITIZEN CENTRAL ',
+    'CHECK_IN MC1 ADULT AIRPORT ',
+    'CHECK_IN MC3 KID AIRPORT ',
+    'CHECK_IN MC4 ADULT AIRPORT ',
+    'CHECK_IN MC5 KID AIRPORT ',
+    'PRINT_SUMMARY ',
+  ];
   it('first output of getArray as data should be array', function () {
     const { inputCardBalance, _ } = getDataAsArray(inputLines);
     assert(Array.isArray(inputCardBalance));
@@ -26,5 +30,49 @@ describe('Unit tests', function () {
     const { _, inputPassengerCardsAndDestinations } =
       getDataAsArray(inputLines);
     assert(Array.isArray(inputPassengerCardsAndDestinations));
+  });
+});
+describe('Unit tests for checkIfReturnJourney', () => {
+  it('Should return false for this input', () => {
+    const expenses = [
+      {
+        card_no: 'MC1',
+        cost: 100,
+        discount: 0,
+        passenger_type: 'SENIOR_CITIZEN',
+        destination: 'AIRPORT',
+      },
+      {
+        card_no: 'MC2',
+        cost: 50,
+        discount: 0,
+        passenger_type: 'KID',
+        destination: 'AIRPORT',
+      },
+    ];
+    const cardNo = 'MC3';
+    const isReturnJourney = checkIfReturnJourney(expenses, cardNo);
+    assert(!isReturnJourney);
+  });
+  it('should return true for this input', () => {
+    const expenses = [
+      {
+        card_no: 'MC1',
+        cost: 100,
+        discount: 0,
+        passenger_type: 'SENIOR_CITIZEN',
+        destination: 'AIRPORT',
+      },
+      {
+        card_no: 'MC2',
+        cost: 50,
+        discount: 0,
+        passenger_type: 'KID',
+        destination: 'AIRPORT',
+      },
+    ];
+    const cardNo = 'MC1';
+    const isReturnJourney = checkIfReturnJourney(expenses, cardNo);
+    assert(isReturnJourney);
   });
 });
